68b422b6b2b0abd439e9a9ca4aa2a1446ed7bdcc
ziyuliu <ziyuliu@umich.edu>
2023-11-02 22:42:36 -0400

    hw_try

---
+++ R/hello.R
@@ -0,0 +1,19 @@
+#'hw_linear_regression
+#
+#'Get the linear regression paramter
+#'
+#'@param X predictors values
+#'@param Y response values
+#'
+#'return the least squares estimates in matrix notation
+#'
+#'@examples
+#'linear_regression(X = matrix(rnorm(3*100), nrow = 100, ncol = 3), Y = matrix(rnorm(1*100), nrow = 100, ncol = 1))
+#'
+#' @export
+#'
+
+
+linear_regression = function (X, Y) {
+  return(solve(t(X) %*% X) %*% t(X) %*% Y)
+}
